6c308341f150de5b6c5e004de43f0424ab7713f5
SlipknotTN <micheletoni1@gmail.com>
2024-02-15 02:29:12 +0100

    3D visualization and comparison WIP

--- visualization/visualize_and_save_3d.py
+++ visualization/visualize_and_save_3d.py
@@ -4,6 +4,8 @@ Visualize 3D shape with 1 or 0 values. XYZ left hand.
 import argparse
 import numpy as np
 import matplotlib.pyplot as plt
+
+import open3d as o3d
 from scipy.ndimage import zoom
 
 
@@ -16,8 +18,9 @@ def do_parsing():
         "--input_file", required=True, type=str, help="Numpy array 3D shaped filepath"
     )
     parser.add_argument(
-        "--output_obj_file", required=False, type=str, help="Output obj filepath"
+        "--output_pcd_file", required=False, type=str, help="Output pcd filepath"
     )
+    # TODO: Add rescale factor
     args = parser.parse_args()
     return args
 
@@ -28,42 +31,46 @@ def main():
 
     print("Loading volume")
     volume = np.load(args.input_file)
-    print(f"3D Shape num_slices x 2D height x 2D width (zyx): {volume.shape}")
+    print(f"Loaded 3D Shape num_slices x 2D height x 2D width (zyx): {volume.shape}")
 
-    rescaled_volume = zoom(volume, (0.1, 0.1, 0.1))
+    print("Rescaling volume")
+    rescaled_volume = zoom(volume, (0.25, 0.25, 0.25))
     print(f"Rescaled 3D shape num_slices x 2D height x 2D width (zyx): {rescaled_volume.shape}")
     del volume
 
-    # FIXME: vertices are not enough
+    # TODO: Use np.indices?!
     x_list = []
     y_list = []
     z_list = []
-    if args.output_obj_file:
-        print(f"Saving obj file to {args.output_obj_file}")
-        with open(args.output_obj_file, 'w') as out_obj:
-            for z in range(rescaled_volume.shape[0]):
-                for y in range(rescaled_volume.shape[1]):
-                    for x in range(rescaled_volume.shape[2]):
-                        if rescaled_volume[z][y][x] > 0.0:
-                            out_obj.write(f"v {x} {y} {z}\n")
-                            x_list.append(x)
-                            y_list.append(y)
-                            z_list.append(z)
-        print(f"obj file saved to {args.output_obj_file}")
+    xyz_values = []
+    for z in range(rescaled_volume.shape[0]):
+        for y in range(rescaled_volume.shape[1]):
+            for x in range(rescaled_volume.shape[2]):
+                if rescaled_volume[z][y][x] > 0.0:
+                    x_list.append(x)
+                    y_list.append(y)
+                    z_list.append(z)
+                    xyz_values.append([x, y, z])
 
-    # Color by red the valid pixels (1.0 values)
-    # print("Visualizing 3D volume as voxels")
-    # colors = np.empty(rescaled_volume.shape, dtype=object)
-    # colors[rescaled_volume > 0.0] = 'red'
-    # ax = plt.figure().add_subplot(projection='3d')
-    # ax.voxels(rescaled_volume, facecolors=colors)
-    # plt.show()
+    # Save point cloud to PCD format
+    xyz_npy = np.array(xyz_values)
+    print(f"XYZ npy shape: {xyz_npy.shape}")
+    if args.output_pcd_file:
+        print(f"Saving PCD file to {args.output_pcd_file}")
+        pcd = o3d.geometry.PointCloud()
+        pcd.points = o3d.utility.Vector3dVector(xyz_npy)
+        o3d.io.write_point_cloud(args.output_pcd_file, pcd)
+        print(f"PCD file save to {args.output_pcd_file}")
+        o3d.visualization.draw_geometries([pcd])
+        # To save custom intensity instead of increasing from bottom to top
+        # pcd.point["positions"] = o3d.core.Tensor(xyz)
+        # pcd.point["intensities"] = o3d.core.Tensor(i)
 
+    # Lighter and quicker visualization than voxels
     print("Visualizing 3D volume as point cloud")
     fig = plt.figure(figsize=(8, 8))
     ax = fig.add_subplot(111, projection='3d')
-    # TODO: Draw smaller points
-    ax.scatter(x_list, y_list, z_list, linewidths=0.1)
+    ax.scatter(x_list, y_list, z_list, s=5)
     plt.show()
 
 
